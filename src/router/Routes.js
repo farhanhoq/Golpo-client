995c9bc04f0c1c563546adacfb9ee3f7ae92da23
Farhan Hoque <farhan@Farhans-MacBook-Pro.local>
2022-11-09 09:49:59 +0600

    Basic Setup Done

---
+++ src/router/Routes.js
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Services from "../pages/services/Services";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+            {
+                path: '/services',
+                element: <Services></Services>
+            },
+        ]
+    }
+])
