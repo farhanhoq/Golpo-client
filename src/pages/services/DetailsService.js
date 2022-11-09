607f806cdd127cb0d6c8a4ccc47eeb3c16c7015f
Farhan Hoque <farhan@Farhans-MacBook-Pro.local>
2022-11-10 00:12:19 +0600

    customer added review load from server

--- src/pages/services/DetailsService.js
+++ src/pages/services/DetailsService.js
@@ -1,21 +1,29 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../context/AuthProvider";
+import Review from "./Review";
 
 const DetailsService = () => {
     const { title, img, description, price, _id } = useLoaderData();
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/reviews")
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, []);
+
     const { user } = useContext(AuthContext);
-    const [review, setReview] = useState([])
 
     const handleReview = e => {
-
+        e.preventDefault();
         const form = e.target;
         const name = form.cname.value;
         const email = user?.email || "Not Found";
         const message = form.message.value;
 
         const review = {
-            review: _id,
+            reviewID: _id,
             cName: name,
             email,
             message
@@ -41,83 +49,43 @@ const DetailsService = () => {
     return (
         <div className="grid grid-cols-6 gap-6">
 
-        <div className="col-span-4">
-            <div className="hero min-h-screen" style={{backgroundImage: `url(${img})`}}>
-                <div className="hero-overlay bg-opacity-60"></div>
-                <div className="hero-content text-center text-neutral-content">
-                    <div className="hero min-h-screen">
-                        <div className="hero-content flex-col lg:flex-row">
-                            <div>
-                                <img src={img} className="max-w-sm rounded-lg shadow-2xl" alt=""/>
-                                <div className="flex">
-                                    <img src={img} className="max-w-sm rounded-lg shadow-2xl w-1/2" alt=""/>
-                                    <img src={img} className="max-w-sm rounded-lg shadow-2xl w-1/2" alt=""/>
+            <div className="col-span-4">
+                <div className="hero min-h-screen" style={{backgroundImage: `url(${img})`}}>
+                    <div className="hero-overlay bg-opacity-60"></div>
+                    <div className="hero-content text-center text-neutral-content">
+                        <div className="hero min-h-screen">
+                            <div className="hero-content flex-col lg:flex-row">
+                                <div>
+                                    <img src={img} className="max-w-sm rounded-lg shadow-2xl" alt=""/>
+                                    <div className="flex">
+                                        <img src={img} className="max-w-sm rounded-lg shadow-2xl w-1/2" alt=""/>
+                                        <img src={img} className="max-w-sm rounded-lg shadow-2xl w-1/2" alt=""/>
+                                    </div>
+                                    <div className="flex">
+                                        <img src={img} className="max-w-sm rounded-lg shadow-2xl w-1/3" alt=""/>
+                                        <img src={img} className="max-w-sm rounded-lg shadow-2xl w-1/3" alt=""/>
+                                        <img src={img} className="max-w-sm rounded-lg shadow-2xl w-1/3" alt=""/>
+                                    </div>
                                 </div>
-                                <div className="flex">
-                                    <img src={img} className="max-w-sm rounded-lg shadow-2xl w-1/3" alt=""/>
-                                    <img src={img} className="max-w-sm rounded-lg shadow-2xl w-1/3" alt=""/>
-                                    <img src={img} className="max-w-sm rounded-lg shadow-2xl w-1/3" alt=""/>
+                                <div>
+                                <h1 className="text-5xl font-bold">{title}</h1>
+                                <p className="py-6">{description}</p>
+                                <button className="btn btn-primary">Book Now</button>
                                 </div>
-                            </div>
-                            <div>
-                            <h1 className="text-5xl font-bold">{title}</h1>
-                            <p className="py-6">{description}</p>
-                            <button className="btn btn-primary">Book Now</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
             <div className="col-span-2">
-
                 <p className="text-center text-4xl my-5">Reviews</p>
-
-                <div>
-                <div className="cardshadow-xl flex flex-row items-center my-2">
-                    
-                    <div className="avatar">
-                        <div className="w-24 h-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 ml-4">
-                            <img src="https://placeimg.com/192/192/people" />
-                        </div>
-                    </div>
-                        
-                    <div className="card-body">
-                        <h2 className="card-title text-sm">"Their Service was too good. Very Professional."</h2>
-                        <p>by Herin Khan</p>
-                    </div>
-                </div>
-
-                <div className="cardshadow-xl flex flex-row items-center my-2">
-                    
-                    <div className="avatar">
-                        <div className="w-24 h-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 ml-4">
-                            <img src="https://placeimg.com/192/192/people" />
-                        </div>
-                    </div>
-                        
-                    <div className="card-body">
-                        <h2 className="card-title text-sm">"Their Service was too good. Very Professional."</h2>
-                        <p>by Herin Khan</p>
-                    </div>
-                </div>
-
-                <div className="cardshadow-xl flex flex-row items-center my-2">
-                    
-                    <div className="avatar">
-                        <div className="w-24 h-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 ml-4">
-                            <img src="https://placeimg.com/192/192/people" />
-                        </div>
-                    </div>
-                        
-                    <div className="card-body">
-                        <h2 className="card-title text-sm">Their Service was too good. Very Professional.</h2>
-                        <p>by Herin Khan</p>
-                    </div>
-                </div>
-                </div>
-
+                {
+                    reviews.map(review => <Review
+                        key={review._id}
+                        review={review}
+                    ></Review>)
+                }
                 <div className="flex justify-center mt-6">
                     <label htmlFor="my-modal-6" className="btn btn-ghost">Give Review</label>
 
---
+++ src/pages/services/Review.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../../context/AuthProvider';
+
+const Review = ({ review }) => {
+    
+    const { _id, reviewID, cName, email, message } = review;
+
+    return (
+        <div>
+
+                <div>
+                <div className="cardshadow-xl flex flex-row items-center my-2">
+                    
+                    <div className="avatar">
+                        <div className="w-10 rounded-full">
+                            <img src="https://placeimg.com/80/80/people" />
+                        </div>
+                    </div>
+                        
+                    <div className="card-body">
+                        <h2 className="card-title text-lg">{message}</h2>
+                        <p className='text-sm'>by Herin Khan</p>
+                    </div>
+                </div>
+                </div>
+
+                
+        </div>
+    );
+};
+
+export default Review;
